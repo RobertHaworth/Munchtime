3909c02bb516b215718a0aa71e9e327973292b58
Robert Haworth <robertchaworth@gmail.com>
2023-08-31 12:38:56 -0500

    Initial Commit

---
+++ Munchtime/MunchtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunchtimeApp.swift
+//  Munchtime
+//
+//  Created by Robert Haworth on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct MunchtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
